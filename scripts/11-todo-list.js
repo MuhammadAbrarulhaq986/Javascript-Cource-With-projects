ee62c3164169ddf7c0490c7d9addf6517bbf2ead
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2025-07-19 13:54:26 +0500

    Practicing Arrays with Loops (While loop, for loop) And Arrays are referances, destructuring also More features of loops Break, continue, loops inside a function

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,6 +1,12 @@
 
 
-const todoList = ["Make dinner", "wash dishes"];
+const todoList = [{
+    name: "Make dinner",
+    dueDate: '2025-19-7'
+}, {
+    name: "Wash dishes",
+    dueDate: '2025-19-7'
+}];
 
 renderTodoList();
 
@@ -8,13 +14,22 @@ function renderTodoList() {
     let todoListHTML = '';
 
     for (let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p> ${todo}</p>`;
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate
+        const { name, dueDate } = todoObject; //* This is called Destructuring
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+        " class="delete-todo-button">Delete</button>
+        `;
         //const html = `<li> ${todo}</li>`;
         todoListHTML += html;
     }
-    console.log(todoListHTML);
-
+    //console.log(todoListHTML); 
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 
@@ -22,8 +37,17 @@ function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value
     //console.log(name);
-    todoList.push(name);
-    console.log(todoList);
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+    todoList.push({
+        //name: name,
+        //dueDate: dueDate
+        //* This is called shorthand property syntax 
+        name,
+        dueDate,
+    });
+    //console.log(todoList);
 
     inputElement.value = '';
 
