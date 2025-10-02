8a9e64c0e58e9eb784bf05e0e0dea9ed18085821
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2025-10-02 18:21:49 +0500

    Some more changes to to the Projects and understanding of Hoisting a function

--- scripts/12-rock-paper-scissor.js
+++ scripts/12-rock-paper-scissor.js
@@ -10,9 +10,9 @@ updateScoreElement();
 let isAutoPlaying = false;
 let intervalId;
 
-const autoPlay = () =>{
+//const autoPlay = () =>{
 
-};
+//}; 
 
 //* Note: When we create a normal function we can use Hoisting
 //* Means we can call the function before creating it 
@@ -28,6 +28,24 @@ function autoPlay() {
         isAutoPlaying = false;
     }
 }
+
+document.querySelector('.js-rock-button').addEventListener("click", () => { playGame("rock") });
+
+document.querySelector('.js-paper-button').addEventListener("click", () => { playGame("paper") });
+
+document.querySelector('.js-scissors-button').addEventListener("click", () => { playGame("scissors") });
+
+document.body.addEventListener('keydown', (event) => {
+    //console.log(event.key);
+    if (event.key === 'r') {
+        playGame('rock');
+    } else if (event.key === 'p') {
+        playGame('paper');
+    } else if (event.key == 's') {
+        playGame('scissors');
+    }
+
+})
 
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -13,22 +13,39 @@ renderTodoList();
 function renderTodoList() {
     let todoListHTML = '';
 
-    todoList.forEach(function (todoObject, index) {
+    //* calling function by name
+    //todoList.forEach(function (todoObject, index) {
+    //    const { name, dueDate } = todoObject; //* This is called Destructuring
+    //    const html = ``;
+    //    //const html = `<li> ${todo}</li>`;
+    //    todoListHTML += html;
+    //});
+    //* calling a function using arrow func
+    todoList.forEach((todoObject, index) => {
         const { name, dueDate } = todoObject; //* This is called Destructuring
         const html = `
         <div>${name}</div>
         <div>${dueDate}</div>
-        <button onclick="
-        todoList.splice(${index}, 1);
-        renderTodoList();
-        " class="delete-todo-button">Delete</button>
+        <button class="delete-todo-button js-delete-todo-button">Delete</button>
         `;
         //const html = `<li> ${todo}</li>`;
         todoListHTML += html;
     });
     //console.log(todoListHTML); 
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+    document.querySelectorAll('.js-delete-todo-button')
+        .forEach((deleteButton, index) => {
+            deleteButton.addEventListener('click', () => {
+                todoList.splice(index, 1);
+                renderTodoList();
+            });
+        });
 }
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+    addTodo();
+});
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
